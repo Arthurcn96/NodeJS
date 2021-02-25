a7995acec617f9a041505d98e5b550eecf4a1d08
Arthurcn96 <arthurcn96@gmail.com>
2021-02-25 13:08:49 -0300

    Finalizando segunda aula

--- API/src/server.ts
+++ API/src/server.ts
@@ -1,24 +1,11 @@
+import 'reflect-metadata'
 import express, { request } from 'express'
+import "./database";
+import { router } from './routes';
 
 const app = express()
 
-/**
- * GET      =>  Busca
- * POST     =>  Salvar
- * PUT      =>  Alterar
- * DELETE   =>  Deletar
- * PATCH    =>  Alteração Especifica
- */
-
-app.get("/", (request, response) => {
-    return response.send("Hello World");
-});
-
-// 1 parametro => ROTA(Recurso API)
-// 2 parametro => Request, Response
-
-app.post("/",  (request, response) => {
-    return response.json({message: "Os dados foram salvos com sucesso!"});
-});
+app.use(express.json());
+app.use(router)
 
 app.listen(3333, () => console.log("Server is Runnign!"));
